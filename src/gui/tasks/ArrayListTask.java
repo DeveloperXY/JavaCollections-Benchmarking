9d7d90171d6c79712ba921cdcce78629b12c8f81
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-03-06 13:58:34 +0000

    Add a label to the CenterLayout to show the generated numbers' sequence.

--- src/gui/tasks/ArrayListTask.java
+++ src/gui/tasks/ArrayListTask.java
@@ -1,13 +1,26 @@
 package gui.tasks;
 
+import java.util.Map;
+import java.util.concurrent.Callable;
+
 /**
  * Created by Mohammed Aouf ZOUAG on 06/03/2016.
  *
  * The ArrayList version.
  */
-public class ArrayListTask implements Runnable {
+public class ArrayListTask implements Callable<Map<String, Double>> {
+
+    private int N;
+    private int M;
+    private long totalRunTime;
+
+    public ArrayListTask(int n, int m) {
+        N = n;
+        M = m;
+    }
+
     @Override
-    public void run() {
+    public Map<String, Double> call() throws Exception {
 
     }
 }
